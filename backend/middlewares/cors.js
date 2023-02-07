898d97ebf87dc49f8d2abc1ae2ae4b5aa132035f
alexeylavrinenkov <lavrinenkovalexey@yandex.ru>
2023-02-07 20:49:57 +0500

    fix: вынес данные для CORS в константы

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,18 +1,16 @@
-const {
-  ALLOWED_CORS = [
-    'https://mesto.service.nomoredomainsclub.ru',
-    'https://api.mesto.service.nomoredomainsclub.ru',
-    'http://mesto.service.nomoredomainsclub.ru',
-    'http://api.mesto.service.nomoredomainsclub.ru',
-    'localhost:3000',
-  ],
-  ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE',
-} = process.env;
+const ALLOWED_CORS = [
+  'https://mesto.service.nomoredomainsclub.ru',
+  'https://api.mesto.service.nomoredomainsclub.ru',
+  'http://mesto.service.nomoredomainsclub.ru',
+  'http://api.mesto.service.nomoredomainsclub.ru',
+  'localhost:3000',
+];
+const ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
-  const { requestHeaders } = req.headers['Access-Control-Request-Headers'];
+  const requestHeaders = req.headers['access-control-request-headers'];
 
   if (ALLOWED_CORS.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);

087ccb895be3cd6eac5ee604b72de4938b1756a7
alexeylavrinenkov <lavrinenkovalexey@yandex.ru>
2023-02-07 22:23:55 +0500

    feat: добавить заглушки для профиля

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Card from './Card';
 import { CurrentUserContext } from '../contexts/CurrentUserContext';
+import backgroundPath from '../images/Black-Background.jpg';
 
 
 function Main(props) {
@@ -10,14 +11,14 @@ function Main(props) {
     <main className="content">
       <section className="profile">
         <div className="profile__container-avatar">
-          <img className="profile__avatar" src={currentUser.avatar} alt="Фотография профиля" />
+          <img className="profile__avatar" src={currentUser.avatar || backgroundPath} alt="Фотография профиля" />
           <button className="profile__avatar-edit-button link" onClick={props.onEditAvatar} type="button" name="avatar-edit-button"></button>
           <div className="profile__info">
             <div className="profile__container-name">
-              <h1 className="profile__name">{currentUser.name}</h1>
+              <h1 className="profile__name">{currentUser.name || 'Имя'}</h1>
               <button className="profile__edit-button link" onClick={props.onEditProfile} type="button" name="edit-button"></button>
             </div>
-            <p className="profile__work">{currentUser.about}</p>
+            <p className="profile__work">{currentUser.about || 'О себе'}</p>
           </div>
         </div>
         <button className="profile__add-button link" onClick={props.onAddPlace} type="button"></button>
